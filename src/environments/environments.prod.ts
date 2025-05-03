c4c243331369d9302e3208df768e0af1773de6f4
ravikumar1719 <ravikumar.sundhar@augmont.com>
2025-05-03 23:22:29 +0530

    update prod env

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     isMockEnabled: true, // You have to switch this, when your real back-end is done
-    api: 'http://172.22.0.2:3000',
+    api: 'https://employee-management-backend-7mqm.onrender.com/',
 };
