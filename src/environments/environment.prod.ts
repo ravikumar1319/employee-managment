f1c0c2dc448561ddb57d032b080d4e150b20ec2e
ravikumar1719 <ravikumar.sundhar@augmont.com>
2025-05-03 23:40:52 +0530

    fix:remove / from env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     isMockEnabled: true, // You have to switch this, when your real back-end is done
-    api: 'https://employee-management-backend-7mqm.onrender.com/',
+    api: 'https://employee-management-backend-7mqm.onrender.com',
 };
