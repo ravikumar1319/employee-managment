fee7b3c74af898c99fac5df8534ea81920ebc9d9
ravikumar1719 <ravikumar.sundhar@augmont.com>
2025-05-06 18:02:53 +0530

    fix added title for login page

--- src/app/config/title.config.ts
+++ src/app/config/title.config.ts
@@ -2,7 +2,10 @@ export const titleConfig: any = {
     "": { title: "Employee List", desc: 'Employee List' },
     "add": { title: "Add Employee ", desc: 'Add Employee ' },
     "reports": { title: "Report List", desc: 'Report List' },
-    "edit": { "id": { title: "Edit Employee", desc: 'Edit Employee' } }
+    "edit": { "id": { title: "Edit Employee", desc: 'Edit Employee' } },
+    "auth":{
+        "login" :{ title: "Login", desc: 'Login' }
+    }
 }
 
 export const getTitleConfigValue = (path: string, fallback: any = { title: 'N/A', desc: '' }) => {
